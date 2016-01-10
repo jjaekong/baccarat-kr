2a51840f264ea8fdff3e0f83ae2c5cafd15f9dff
Kim Jaehong <jjaekong@gmail.com>
2016-01-11 00:28:56 +0900

    .

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Baccarat Fine Crystal Jewelry, Lighting &amp; Gifts for Special Occasions - Korean Official Site</title>
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+<link href="/css/default.css" rel="stylesheet">
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+    <div id="wrapper">
+        <header id="header">
+            <h1 class="logo"><a href="#"><img src="/images/logo.gif" alt="Baccarat"></a></h1>
+            <button type="button" class="btn btn-search" aria-label="검색">
+                <span class="glyphicon glyphicon glyphicon-search" aria-hidden="true"></span>
+            </button>
+            <button type="button" class="btn btn-menu" aria-label="메뉴">
+                <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+            </button>
+        </header>
+        <footer id="footer">
+            
+        </footer>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
