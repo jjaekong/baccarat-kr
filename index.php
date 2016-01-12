63f7d9d15d99cd3de9142319e4782c93f3983300
김재홍 <jjaekong@gmail.com>
2016-01-12 13:44:25 +0900

    상단, 하단 작업 중..

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Baccarat Fine Crystal Jewelry, Lighting &amp; Gifts for Special Occasions - Korean Official Site</title>
+<link href="//cdn.jsdelivr.net/font-nanum/1.0/nanumbarungothic/nanumbarungothic.css" rel="stylesheet">
+<link href="//cdn.jsdelivr.net/font-nanum/1.0/nanumbarungothic/nanumbarungothic.css" rel="stylesheet">
+<link href="//cdn.jsdelivr.net/font-nanum/1.0/nanummyeongjo/nanummyeongjo.css" rel="stylesheet">
 <link href="/css/bootstrap.min.css" rel="stylesheet">
 <link href="/css/default.css" rel="stylesheet">
 <!--[if lt IE 9]>
@@ -22,9 +25,40 @@
             <button type="button" class="btn btn-menu" aria-label="메뉴">
                 <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
             </button>
+            <nav>
+            </nav>
         </header>
+        <nav>
+        </nav>
         <footer id="footer">
-            
+            <nav>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-4"><a href="#">고객센터</a></div>
+                        <div class="col-xs-4"><a href="#">매장찾기</a></div>
+                        <div class="col-xs-4"><a href="#">PC버전 보기</a></div>
+                    </div>
+                </div>
+            </nav>
+            <div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <address>서울시 용산구 소월로 70 메종 마카라</address>
+                            <span role="separator">|</span>
+                            <dl>
+                                <dt>Tel</dt>
+                                <dd>02-3448-3778</dd>
+                            </dl>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p>© Baccarat Jewelry. All Reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
