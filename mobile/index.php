e9725a3e4d381bef5cae6e3385d65cfb9c089871
김재홍 <jjaekong@gmail.com>
2016-01-18 13:08:22 +0900

    메인 페이지 작성 중...

--- mobile/index.php
+++ mobile/index.php
@@ -1,10 +1,78 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/css/main.css" rel="stylesheet">
 </head>
-<body>
+<body class="main">
     <div id="wrapper">
         <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
         <main id="content">
-            #content
+            <div class="overview">
+                <figure>
+                    <img class="img-responsive" src="/mobile/images/main/img_overview.jpg">
+                    <figcaption class="sr-only">
+                        Baccarat Best wishes for a happy new year 2016
+                    </figcaption>
+                </figure>
+            </div>
+            <section class="features">
+                <div class="container">
+                    <a href="#">
+                        <div class="figure"></div>
+                        <div class="details">
+                            <img class="gift"src="/mobile/images/main/ico_gift_grey.jpg" aria-hidden="true">
+                            <h4>크리스탈의 전설, 메종 바카라 서울 남산에 오픈</h4>
+                            <address><p>서울시 용산구 소월로 70</p></address>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="details">
+                            <img class="gift"src="/mobile/images/main/ico_gift_white.jpg" aria-hidden="true">
+                            <h4>우아함이 돋보이는 홈 데코레이션</h4>
+                            <hr>
+                            <p>바카라가 제안하는 데코레이션 컬렉션</p>
+                        </div>
+                        <div class="figure"></div>
+                    </a>
+                    <a href="#">
+                        <div class="figure"></div>
+                        <div class="details">
+                            <img class="gift" src="/mobile/images/main/ico_gift_white.jpg" aria-hidden="true">
+                            <h4>홀리데이 무드</h4>
+                            <hr>
+                            <p>바카라의 크리스마스 셀렉션으로 테이블을 장식하세요</p>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="details">
+                            <h4>바카라의새로운 제품을 만나보세요.</h4>
+                            <p>카페 바카라, 나누고 싶은 특별한 경험</p>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="details">
+                            <img class="gift" src="/mobile/images/main/ico_gift_white.jpg" aria-hidden="true">
+                            <h4>완벽한 데코레이션</h4>
+                            <hr>
+                            <p>모두를 위한 기프트 세트</p>
+                        </div>
+                        <div class="figure"></div>
+                    </a>
+                    <a href="#">
+                        <div class="figure"></div>
+                        <div class="details">
+                            <img class="gift" src="/mobile/images/main/ico_gift_white.jpg" aria-hidden="true">
+                            <h4>무드 체인지</h4>
+                            <hr>
+                            <p>모든 여성을 위한 완벽한 주얼리</p>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="details">
+                            <h4>눈부신 바카라 라이팅 제품</h4>
+                            <p>지금, 바카라의 빛나는컬렉션을 만나보세요</p>
+                        </div>
+                    </a>
+                </div>
+            </section>
         </main>
         <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
     </div>
