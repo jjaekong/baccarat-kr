b017f8b8418416d115aa0b9937642ecf5f680afb
Kim Jaehong <jjaekong@gmail.com>
2016-01-24 22:37:39 +0900

    바카라의 세계 > 바카라의 노하우 목록, 작업 완료

---
+++ mobile/world/knowhow_list.php
@@ -0,0 +1,84 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+<link href="/mobile/css/world.css" rel="stylesheet">
+</head>
+<body class="world">
+    <div id="wrapper">
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+        <main id="content" class="knowhow">
+            <div class="container">
+                <div class="path">
+                    <ol class="breadcrumb">
+                        <li><a href="#">HOME</a></li>
+                        <li class="active"><a href="#">바카라의 노하우</a></li>
+                    </ol>
+                </div>
+                <div class="list">
+                    <ul class="row">
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/mobile/images/dummy/img_knowhow_1.jpg">
+                                    <figcaption>유서깊은 향수 그리고 향수병, ROUGE 540(루즈 540)</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/mobile/images/dummy/img_knowhow_2.jpg">
+                                    <figcaption>유서깊은 향수 그리고 향수병</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="clearfix"></li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/mobile/images/dummy/img_knowhow_3.jpg">
+                                    <figcaption>유서깊은 향수 그리고 향수병</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/mobile/images/dummy/img_knowhow_4.jpg">
+                                    <figcaption>유서깊은 향수 그리고 향수병, ROUGE 540(루즈 540)</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="clearfix"></li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/mobile/images/dummy/img_knowhow_5.jpg">
+                                    <figcaption>유서깊은 향수 그리고 향수병, ROUGE 540(루즈 540)</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/mobile/images/dummy/img_knowhow_6.jpg">
+                                    <figcaption>유서깊은 향수 그리고 향수병, ROUGE 540(루즈 540)</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="clearfix"></li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/mobile/images/dummy/img_knowhow_1.jpg">
+                                    <figcaption>유서깊은 향수 그리고 향수병, ROUGE 540(루즈 540)</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+    </div>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
